54a38215a9ec90e59c72b278d6d43344641f5adc
Markanday Ravi <markandayravi@gmail.com>
2017-11-03 09:16:13 -0700

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -8,6 +8,7 @@ public class Driver extends JApplet{
 	public void init()
 	{
 		 createPanel = new CreatePanel();
+		 getContentPane().add(createPanel);
 		 setSize (APPLET_WIDTH, APPLET_HEIGHT); //set Applet size
 	}
 }
